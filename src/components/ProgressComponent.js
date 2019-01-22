33f5b8002bec9989f9d37f15d179d82bf9ad7931
D Guardemi <dmitryarchebasov@gmail.com>
2019-01-22 17:37:59 +0300

    [task]  структура

---
+++ src/components/ProgressComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProgressStepComponent from './ProgressStepComponent'
+
+function ProgressComponent () {
+  return (
+    <div>
+      <ProgressStepComponent/>
+      <ProgressStepComponent/>
+      <ProgressStepComponent/>
+    </div>
+  )
+}
+
+export default ProgressComponent
