a6615366b703e7bf0698cc0f398277cea8db5d2a
D Guardemi <dmitryarchebasov@gmail.com>
2019-01-22 20:47:20 +0300

    [task] наброски стилей

--- src/components/ProgressComponent.js
+++ src/components/ProgressComponent.js
@@ -2,19 +2,78 @@ import React from 'react'
 import ProgressStepComponent from './ProgressStepComponent'
 import styled from 'styled-components'
 
+const COLOR_ACTIVE = '#5132da';
+
 const Progress = styled.div`
   display: flex;
-  width: 100%;
+  position: relative;
+  width: 400px;
+  justify-content: space-between;
 `;
 
-function ProgressComponent () {
-  return (
-    <Progress>
-      <ProgressStepComponent/>
-      <ProgressStepComponent/>
-      <ProgressStepComponent/>
-    </Progress>
-  )
+const LinkBettweenSteps = styled.div`
+  width: auto;
+  height: 10px;
+  flex: 1;
+  background-color: ${props => props.complete ? COLOR_ACTIVE : 'gray'}
+`;
+
+const Step = styled.div`
+  display: flex;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const tasks = [
+  {
+    title: 'Design',
+    complete: true,
+  },
+  {
+    title: 'Build',
+    complete: false,
+  },
+  {
+    title: 'Launch',
+    complete: false,
+  },
+];
+
+class ProgressComponent extends React.Component {
+
+  render() {
+    let steps = [];
+    let linksBetweenSteps = [];
+    tasks.forEach((task, index) => {
+      if (index > 0) {
+        linksBetweenSteps.push(<LinkBettweenSteps complete={task.complete}/>)
+      }
+      steps.push(
+        <Step key={index.toString()}>
+          <ProgressStepComponent
+            title={task.title}
+            complete={task.complete}
+          />
+        </Step>
+      )
+      }
+    );
+
+    return (
+      <Progress>
+        <Background>
+          {linksBetweenSteps}
+        </Background>
+        {steps}
+      </Progress>
+    )
+  }
 }
 
 export default ProgressComponent
--- src/components/ProgressStepComponent.js
+++ src/components/ProgressStepComponent.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const COLOR_ACTIVE = '#5132da';
+
+const StatusBar = styled.div`
+  border: 10px solid ${COLOR_ACTIVE};
+  border-radius: 50%;
+  background-color: white;
+  width: 30px;
+  height: 30px; 
+`;
+
+const ProgressStep = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  color: ${COLOR_ACTIVE};
+  font-size: 20px;
+  
+`;
 
 export default () => {
   return (
-    <div>
-      <div>Name</div>
-      <div />
-    </div>
+    <ProgressStep>
+      <Title>Name</Title>
+      <StatusBar />
+    </ProgressStep>
   )
 }
