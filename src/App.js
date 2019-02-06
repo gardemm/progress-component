2e7a301537b6263c36e97e06ab05307146a3d1c3
D Guardemi <dmitryarchebasov@gmail.com>
2019-02-06 20:09:27 +0300

    [task] исключение в eslint для reduxDevTools

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+// eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(rootReducer, composeEnhancers(
