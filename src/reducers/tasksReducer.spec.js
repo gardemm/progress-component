b76e3dde8ad797550ebdb04e3fc524cb6a57f657
D Guardemi <dmitryarchebasov@gmail.com>
2019-02-14 19:52:37 +0300

    [task] вынес расширения для expect в хелпер

--- src/reducers/tasksReducer.spec.js
+++ src/reducers/tasksReducer.spec.js
@@ -4,25 +4,10 @@ import thunk from 'redux-thunk'
 import reducer, { tasksInitialState } from './tasksReducer'
 import { addTaskAction, deleteTaskAction } from '../actions/tasksActions'
 import type { tasksStateType, taskType } from './tasksReducer'
+import expect from '../helpers/expect'
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
-
-expect.extend({
-    toBeWithinRange(received, floor, ceiling = '') {
-        const pass = received >= floor && received <= ceiling
-        if (pass) {
-            return {
-                message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
-                pass: true,
-            }
-        }
-        return {
-            message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
-            pass: false,
-        }
-    },
-})
 
 // reducer
 describe('tasks reducer', () => {
