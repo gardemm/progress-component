654c050b75fa698603f6ef7f6c20c1b78786fbdf
D Guardemi <dmitryarchebasov@gmail.com>
2019-02-14 19:42:39 +0300

    [task] ErrorBoundary - переопределил state в зависимости от props

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -3,7 +3,7 @@ import React from 'react'
 class ErrorBoundary extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { hasError: false }
+        this.state = { hasError: props.hasError }
     }
 
     static getDerivedStateFromError() {
