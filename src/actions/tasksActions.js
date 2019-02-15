2764a30e7a12c3a1204bec377f30a7946c854be3
D Guardemi <dmitryarchebasov@gmail.com>
2019-02-15 04:08:41 +0300

    [task] условие на toggleTasks - последовательно выполнять задачи

--- src/actions/tasksActions.js
+++ src/actions/tasksActions.js
@@ -4,12 +4,52 @@ import { createAction } from '../helpers/reduxHelper'
 export const toggleTaskAction = (taskId) => (dispatch, getState) => {
     const { tasks: { list } } = getState()
 
-    const newTaskList = list.map(task => ((task.id === taskId)
-        ? { ...task, complete: !task.complete }
-        : task))
+    const newListObj = { list }
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].id === taskId) {
+            if (!list[i].complete) { // задача не завершена, завершаем
+                // первый элемент с остальными не активными
+                if (i === 0 && !list[i + 1].complete && !list[i].complete) {
+                    // активируем
+                    newListObj.list[i].complete = !newListObj.list[i].complete
+                }
+                // если задача активировать, то если текущий элемент отрицательный,
+                // а предыдущий положительный, то активируем в плюс/
+                if (i > 0 && list[i - 1].complete && !list[i].complete) {
+                    newListObj.list[i].complete = !newListObj.list[i].complete
+                }
+            } else {
+                // если задача отменить, то если следующий элемент отрицательный,
+                // а текущий положительный, то сбрасываем
 
-    dispatch(createAction(TOGGLE_TASK)({
-        list: newTaskList,
+                // если не последний, то проверяем как на условии выше
+                if (i < list.length - 1 && list[i].complete && !list[i + 1].complete) {
+                    newListObj.list[i].complete = !newListObj.list[i].complete
+                }
+
+                if (i === list.length - 1 && list[i].complete) {
+                    newListObj.list[i].complete = !newListObj.list[i].complete
+                }
+            }
+
+            // если задача отменить, то если следующий элемент отрицательный,
+            // а этот положительный, то сбрасываем
+        }
+    }
+    // const newTaskList = list.map(task => {
+    //     const lastItemWasCompleted = true
+    //
+    //     if (lastItemWasCompleted) {
+    //
+    //     }
+    //
+    //     return (task.id === taskId)
+    //         ? { ...task, complete: !task.complete }
+    //         : task
+    // })
+
+    return dispatch(createAction(TOGGLE_TASK)({
+        ...newListObj,
     }))
 }
 
@@ -24,7 +64,7 @@ export const addTaskAction = (taskTitle) => (dispatch, getState) => {
 
     const newTaskList = list.concat(task)
 
-    dispatch(createAction(ADD_TASK)({
+    return dispatch(createAction(ADD_TASK)({
         list: newTaskList,
     }))
 }
@@ -35,7 +75,7 @@ export const deleteTaskAction = (taskId) => (dispatch, getState) => {
 
     const newTaskList = list.filter(task => task.id !== taskId)
 
-    dispatch(createAction(DELETE_TASK)({
+    return dispatch(createAction(DELETE_TASK)({
         list: newTaskList,
     }))
 }
